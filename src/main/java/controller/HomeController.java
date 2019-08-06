382a9dfb01b2995261e3cb26f715027ffd97b56a
purechild <a01056490114@gmail.com>
2019-08-06 09:19:35 +0900

    url mapping & jstl test

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -1,6 +1,8 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,5 +10,11 @@ public class HomeController {
     @RequestMapping("/")
     public String showHome(){
         return "home";
+    }
+
+    @GetMapping("/test")
+    public String getTestPage(ModelMap model){
+        model.addAttribute("testData", "test");
+        return "testPage";
     }
 }
---
+++ web/views/testPage.jsp
@@ -0,0 +1,15 @@
+<%--
+  User: SeungsooLee
+  Date: 2019-08-05
+  Time: 오후 10:14
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+${testData}
+</body>
+</html>
