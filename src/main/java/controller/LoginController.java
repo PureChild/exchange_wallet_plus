4ea566426b00bd3d8b448baec2cea36cb88e2782
purechild <a01056490114@gmail.com>
2019-08-10 15:13:31 +0900

    modify login login

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import service.LoginService;
 
 import javax.servlet.http.HttpServletRequest;
@@ -18,10 +19,17 @@ public class LoginController {
     private LoginService loginService;
 
     @PostMapping("/admin/loginConfirm")
-    public String loginAdmin(HttpSession session, @RequestParam String admId, @RequestParam String admPassword){
-        String[] resultLoginCheck = loginService.checkAdminLoginInfo(admId, admPassword);
-        session.setAttribute("msg", resultLoginCheck[1]);
-        return resultLoginCheck[0];
+    public String loginAdmin(RedirectAttributes redirectAttributes, @RequestParam String admId, @RequestParam String admPassword){
+        String resultLoginCheck = loginService.checkAdminLoginInfo(admId, admPassword);
+        redirectAttributes.addFlashAttribute("msg", resultLoginCheck);
+
+        String returnUrl;
+        if(resultLoginCheck.equals("loginOK")){
+            returnUrl = "redirect:/admin/reservation/history";
+        } else {
+            returnUrl = "redirect:/admin/login";
+        }
+        return returnUrl;
     }
 
     @PostMapping("/joinConfirm")
@@ -32,10 +40,16 @@ public class LoginController {
     }
 
     @PostMapping("/loginConfirm")
-    public String login(HttpSession session, @ModelAttribute Customer customer){
-        String[] resultLoginCheck = loginService.checkLoginInfo(customer);
-        session.setAttribute("msg", resultLoginCheck[1]);
+    public String login(RedirectAttributes redirectAttributes, @ModelAttribute Customer customer){
+        String resultLoginCheck = loginService.checkLoginInfo(customer);
+        redirectAttributes.addFlashAttribute("msg", resultLoginCheck);
 
-        return resultLoginCheck[0];
+        String returnUrl;
+        if(resultLoginCheck.equals("loginOK")){
+            returnUrl = "redirect:/";
+        } else {
+            returnUrl = "redirect:/login";
+        }
+        return returnUrl;
     }
 }
--- src/main/java/service/LoginService.java
+++ src/main/java/service/LoginService.java
@@ -17,32 +17,27 @@ public class LoginService {
     @Autowired
     private DaoFactory daoFactory;
 
-    public String[] checkAdminLoginInfo(String admId, String admPassword) {
-        String returnPage, returnMsg;
+    public String checkAdminLoginInfo(String admId, String admPassword) {
+        String returnMsg;
         if(this.admId.equals(admId) && this.admPassword.equals(admPassword)){
-            returnPage = "redirect:/admin/reservation/history";
             returnMsg = "loginOK";
         } else {
-            returnPage = "redirect:/admin/login";
             returnMsg = "관리자 아이디와 비밀번호를 확인해주세요";
         }
-        return new String[]{returnPage, returnMsg};
+        return returnMsg;
     }
 
     public void addCustomer(Customer customer) {
         daoFactory.getCustomerDao().insertCustomer(customer);
     }
 
-    public String[] checkLoginInfo(Customer customer) {
-        String returnPage, returnMsg;
-
+    public String checkLoginInfo(Customer customer) {
+        String returnMsg;
         if(daoFactory.getCustomerDao().selectExistsCustomer(customer)){
-            returnPage = "redirect:/";
             returnMsg = "loginOK";
         } else {
-            returnPage = "redirect:/login";
             returnMsg = "아이디와 비밀번호를 확인해주세요";
         }
-        return new String[]{returnPage, returnMsg};
+        return returnMsg;
     }
 }
