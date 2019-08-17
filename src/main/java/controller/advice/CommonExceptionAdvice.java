0aa8453559d0416e2f92985db0abe261c05f9d48
purechild <a01056490114@gmail.com>
2019-08-17 14:46:27 +0900

    handle connection exception

--- src/main/java/controller/advice/CommonExceptionAdvice.java
+++ src/main/java/controller/advice/CommonExceptionAdvice.java
@@ -1,14 +1,16 @@
 package controller.advice;
 
-import org.springframework.http.HttpStatus;
+import com.mysql.cj.exceptions.CJCommunicationsException;
+import com.mysql.cj.jdbc.exceptions.CommunicationsException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.net.ConnectException;
+import java.sql.SQLException;
 
 @ControllerAdvice(annotations = Controller.class)
 public class CommonExceptionAdvice {
@@ -22,12 +24,23 @@ public class CommonExceptionAdvice {
     }
 
     @ExceptionHandler(BindException.class)
-    public ModelAndView handleBindException(Exception exception, HttpServletRequest request) {
+    public ModelAndView handleBindException(BindException exception, HttpServletRequest request) {
         return showErrorPage(exception, request, "잘못된 요청입니다.");
     }
 
     @ExceptionHandler(NumberFormatException.class)
-    public ModelAndView handleNumberFormatException(Exception exception, HttpServletRequest request) {
+    public ModelAndView handleNumberFormatException(NumberFormatException exception, HttpServletRequest request) {
         return showErrorPage(exception, request, "숫자 형식이 이상합니다.");
+    }
+
+    @ExceptionHandler(SQLException.class)
+    public ModelAndView handleSQLException(SQLException exception, HttpServletRequest request) {
+        String msg;
+        if(exception.getCause().toString().contains("")){
+            msg = "데이터베이스 연결에 실패했습니다.";
+        } else {
+            msg = "";
+        }
+        return showErrorPage(exception, request, msg);
     }
 }
